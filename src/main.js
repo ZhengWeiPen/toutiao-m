d22bab0a2e43cc43723722abaef9bfa9f35910c9
zwp <1556249350@qq.com>
2022-09-08 15:09:56 +0800

    init：引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入Vant组件
+import { Button } from 'vant'
+import 'vant/lib/button/style'
+// 导入全局样式
 import './styles/index.less'
+
+// 注册Vant组件
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
